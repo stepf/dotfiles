880756c802e1a1cc1e6a6c353bc24ab0585e52bd
stepf <misc-github@stefandang.com>
2024-07-14 13:09:37 +0200

    [zsh] refactor: remove installation of zsh plugins from script in favor of Brewfile

--- home/setup.sh
+++ home/setup.sh
@@ -4,11 +4,6 @@ set -o pipefail
 set -e
 set -x
 
-### Install zsh plugins
-mkdir -p "$HOME/.zsh"
-git clone --depth 1 "https://github.com/zsh-users/zsh-syntax-highlighting.git" "$HOME/.zsh/zsh-syntax-highlighting"
-git clone --depth 1 "https://github.com/zsh-users/zsh-autosuggestions.git" "$HOME/.zsh/zsh-autosuggestions"
-
 ### Simlink dotfiles to home directory
 for file in *; do
   ln -s "$(pwd)/$file" "$HOME/.$file"
