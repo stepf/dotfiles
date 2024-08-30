c4def0938683748c4ddfce26b4390727a553e1a2
stepf <misc-github@stefandang.com>
2024-08-30 16:07:13 +0200

    [macos] feature: set further settings in setup.sh

--- macos/setup.sh
+++ macos/setup.sh
@@ -7,11 +7,34 @@ set -x
 # Install XCode Command Line Tools
 xcode-select --install
 
-# Set Finder settings
+# Dock settings
+defaults write com.apple.dock "orientation" -string "left"
+defaults write com.apple.dock "tilesize" -int "48"
+defaults write com.apple.dock "autohide" -bool "true"
+defaults write com.apple.dock "show-recents" -bool "false"
+defaults write com.apple.dock "expose-group-apps" -bool "true" # Group apps in Mission Control
+killall Dock
+
+# Finder settings
 defaults write com.apple.finder CreateDesktop -bool false # Hide Desktop Icons
-defaults write com.apple.finder ShowPathbar -bool true    # Show Pathbar
-defaults write com.apple.finder ShowStatusBar -bool true  # Show Statusbar
+defaults write com.apple.finder ShowPathbar -bool true
+defaults write com.apple.finder ShowStatusBar -bool true
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+defaults write com.apple.finder "FXPreferredViewStyle" -string "Nlsv" # Use list view
+chflags nohidden ~/Library
 killall Finder
+
+# Keyboard settings: Globe key switches input sources
+defaults write com.apple.HIToolbox AppleFnUsageType -int "1"
+
+# Trackpad settings: map bottom right corner to right-click
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadCornerSecondaryClick -int 2
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -bool true
+
+# Set language and text formats
+defaults write NSGlobalDomain AppleLanguages -array "en-DE" "de-DE"
+defaults write NSGlobalDomain AppleLocale -string "en_DE@currency=EUR"
+killall cfprefsd
 
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
